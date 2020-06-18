51a8e97b3ad33ac5ac73e49622a02030579a1477
ernesthenry <henry38ernest@gmail.com>
2020-06-18 08:33:15 +0300

    responsiveness

--- src/App/components/History.js
+++ src/App/components/History.js
@@ -1,33 +1,25 @@
 import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import worship from './images/worship.jpg'
 
 const History = () => {
     return(
-        <div>
-            <Container className="history">
-                <Row>
-                    <Col className='col-xs-6'>
-                    <img  src={worship} alt="images"/>
-                    
-                    </Col>
+        <div className='container history'>
+            <div className='col-xs-3 col'>
 
-                    <Col className='col-xs-6'>
-                    <h2 className="text-center">History </h2>
-                    <p>C3 Alive  is a church that is fresh, real, powerful,and has
+            </div>
+                <div className='col-xs-6 col-6' >
+                    <p className='text-center'>C3 Alive  is a church that is fresh, real, powerful,and has
                     at its heart the Great Commission to win souls and make 
                     disciples of all nations. A church that impacts the city and 
                     influences the nations towards Jesus Christ. A church that has
                      a culture and atmosphere of faith, hope, and love. A church that
                     develops leaders and empowers people to become all that God had 
-                    purposed and destined for their lives.</p>
-                    
+                    purposed and destined for their lives.
+                    </p>
+                </div >
 
-                    </Col>
-                </Row>
-            </Container>
+                <div className='col-xs-3 col'>
+
+                </div>
 
 
         </div>
