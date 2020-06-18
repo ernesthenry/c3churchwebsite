f61965ca3eb695af72f2440d49ccdbf67f18a5e1
ernesthenry <henry38ernest@gmail.com>
2020-06-18 08:39:04 +0300

    responsiveness

--- src/App/components/History.js
+++ src/App/components/History.js
@@ -3,21 +3,22 @@ import React from 'react'
 const History = () => {
     return(
         <div className='container history'>
-            <div className='col-xs-3 col'>
+            <div className='row'>
 
+            <div className='col-xs-3 col-md-3'>
             </div>
-                <div className='col-xs-6 col-6' >
+
+                <div className='col-xs-6 col-md-6' >
                     <p className='text-center'>C3 Alive  is a church that is fresh, real, powerful,and has
                     at its heart the Great Commission to win souls and make 
                     disciples of all nations. A church that impacts the city and 
                     influences the nations towards Jesus Christ. A church that has
-                     a culture and atmosphere of faith, hope, and love. A church that
-                    develops leaders and empowers people to become all that God had 
-                    purposed and destined for their lives.
+                     a culture and atmosphere of faith, hope, and love. 
                     </p>
                 </div >
 
-                <div className='col-xs-3 col'>
+                <div className='col-xs-3 col-md-3'>
+                </div>
 
                 </div>
 
